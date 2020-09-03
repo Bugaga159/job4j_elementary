9f1f1f58ca7a8895bb36bc31f18a3839008fcc64
Панин Андрей Валерьевич <AVPanin@alfabank.ru>
2020-09-03 09:19:03 +0300

    4.2. Тернарное сравнение [#352278] - исправление

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -2,11 +2,7 @@ package ru.job4j.condition;
 
 public class Max {
 	public static int max(int left, int right) {
-		if (left == right) {
-			return left;
-		}
-
-		boolean max = left > right;
+		boolean max = left >= right;
 		int result = max ? left : right;
 		return result;
 	}
