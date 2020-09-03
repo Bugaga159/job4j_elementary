8dd44d5ce9231fdaeef8f4a42a95fb61bd2cb082
Панин Андрей Валерьевич <AVPanin@alfabank.ru>
2020-09-03 22:13:20 +0300

    5.7. Ипотека [#352375]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+	public static double getSumWithPercent(double amount, double percent) {
+		return amount + (amount * percent / 100);
+	}
+
+	public static int year(int amount, int salary, double percent) {
+		int year = 1;
+		double firstYear = getSumWithPercent((double) amount, percent);
+		while (firstYear > salary) {
+			year++;
+			firstYear = getSumWithPercent(firstYear, percent) - salary;
+		}
+		return year;
+	}
+}
