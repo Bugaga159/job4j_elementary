e701154846cfd153eed2edfbd78ddf6f6b99203a
Панин Андрей Валерьевич <AVPanin@alfabank.ru>
2020-09-04 08:32:06 +0300

    5.5. Простое число [#352376] - правки Добавил условия проверки на числа <=1 и один новый тест

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,13 +2,16 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
 	public static boolean check(int number) {
-		boolean prime = false;
-		for (int i = 2; i <= number; i++) {
-			if (i != number && number % i == 0) {
-				prime = false;
-				break;
+		boolean prime = true;
+		if (number <= 1) {
+			prime = false;
+		} else {
+			for (int i = 2; i < number; i++) {
+				if (number % i == 0) {
+					prime = false;
+					break;
+				}
 			}
-			prime = true;
 		}
 		return prime;
 	}
